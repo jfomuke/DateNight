ad5301387be8d536840b8db5ce7fc0154cca5825
Elizabeth Kiser <ekiser9@uncc.edu>
2019-10-31 01:07:54 +0000

    Updated Activity controller - updated new, added create

--- app/controllers/activity_controller.rb
+++ app/controllers/activity_controller.rb
@@ -1,4 +1,11 @@
 class ActivityController < ApplicationController
     def new
+        @activty = Activity.new
+    end
+    
+    def create
+        @activity = Activity.new(params[:activity])
+        
+        @activity.save
     end
 end
