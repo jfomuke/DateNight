8027b6fa0effd1f13eb66511fe96d4f7213214a8
Elizabeth Kiser <ekiser9@uncc.edu>
2019-10-31 19:18:43 +0000

    Added Create, Index, and Show method skelletons

--- app/controllers/activity_controller.rb
+++ app/controllers/activity_controller.rb
@@ -1,11 +1,24 @@
 class ActivityController < ApplicationController
+    def index
+        @activity = Activity.all
+    end
+    
+    def show
+        @activity = Activity.find(params[:activityID])
+    end
+    
     def new
         @activity = Activity.new
     end
     
     def create
-        @activity = Activity.new(params[:activity])
+        @activity = Activity.new(article_params)
         
         @activity.save
     end
 end
+
+private
+    def activity_params
+        params.require(:activity).permit(:activityID, :name, :type, :description, :link, :location)
+    end
