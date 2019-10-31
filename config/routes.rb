53b9ac297a45a07c929b54668d7008dbea5d6014
jcruz23 <jcruz23@uncc.edu>
2019-10-31 17:31:43 +0000

    rename home view file name to index.thml

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   
   resources :activity
   
-  root 'date_night#view'
+  root 'date_night#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
