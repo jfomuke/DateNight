70a29f701f6dc89cefcc19809ce1e3a1a9049702
jcruz23 <jcruz23@uncc.edu>
2019-10-31 17:34:26 +0000

    change in home view

---
+++ app/controllers/user_submission_controller.rb
@@ -0,0 +1,2 @@
+class UserSubmissionController < ActivityController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'date_night/generate'
   
   resources :activity
+  resources :usersubmission
   
   root 'date_night#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
