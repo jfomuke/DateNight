a02af43960c51263e2e3ee98024cceda09e14fc0
Jason Fomuke <jfomuke@uncc.edu>
2019-11-07 18:50:44 +0000

    Staging for pull, CRUD for USub

--- app/controllers/date_night_controller.rb
+++ app/controllers/date_night_controller.rb
@@ -1,4 +1,12 @@
 class DateNightController < ApplicationController
+  
   def generate
   end
+  
+  def create
+        render plain: params[:activity].inspect
+  end
+  
+  
+  
 end
--- app/controllers/user_submission_controller.rb
+++ app/controllers/user_submission_controller.rb
@@ -1,2 +1,46 @@
 class UserSubmissionController < ActivityController
-end
+    #needs update & delete
+    # edit from blog - notes
+    
+    
+     def index
+        @usersubmission = UserSubmission.all
+     end
+    
+    def show
+        @usersubmission = UserSubmission.find(params[:id])
+    end
+    
+    def new
+        @usersubmission = UserSubmission.new
+    end
+    
+    # new (11/7/2019)
+    # usersubmission#edit
+    def edit
+        @usersubmission = UserSubmission.find(params[:id])
+    end
+    # new
+    def destroy
+        @usersubmission = UserSubmission.find(params[:id])
+        @usersubmission.destroy
+        
+        redirect_to articles_path
+    end
+    
+    def create
+        @usersubmission = UserSubmission.new(uSub_params)
+        
+        if @usersubmission.save
+            redirect_to @usersubmission
+        else
+            render 'new'
+        end
+    end
+end # end class
+
+
+private
+    def uSub_params
+        params.require(:usersubmission).permit(:name, :category, :description, :link, :location)
+    end
