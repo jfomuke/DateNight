e1db87082b14b576177e3b4c73f5dacd8a374ffb
Elizabeth Kiser <ekiser9@uncc.edu>
2019-11-08 16:13:43 +0000

    Moved DateNight generating functionality to Generate controller

--- app/controllers/date_night_controller.rb
+++ app/controllers/date_night_controller.rb
@@ -1,17 +1,6 @@
 class DateNightController < ApplicationController
   
-  def show
-    @date_night = Datenight.find(params[:id])
-  end
-
-  def new
-    @date_night = Datenight.new
-  end
-  
-  def create
-    @date_night = Datenight.new(datenight_params)
-    
-    redirect_to @date_night
+  def index
   end
   
 end
--- app/controllers/generator_controller.rb
+++ app/controllers/generator_controller.rb
@@ -1,3 +1,14 @@
 class GeneratorController < ApplicationController
+    def new
+        @generator = Generator.new
+    end
     
+    def show
+        @generator = Generator.find(params[:id])
+    end
+    
+    def create
+        @generator = Generator.new
+        redirect_to @generator
+    end
 end
