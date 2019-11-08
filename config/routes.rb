7105eae1f0bdedcb071daa36c1a4dc7d01870698
Elizabeth Kiser <ekiser9@uncc.edu>
2019-11-08 18:00:16 +0000

    Copied Activity views for User Submission; Updated User Submission controller for routing purposes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   end
   
   resources :activity
-  resources :usersubmission
+  resources :user_submission
   
   root 'date_night#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
