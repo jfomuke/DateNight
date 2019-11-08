b97a7e5107167381df8a417562c0cd48097e6067
Elizabeth Kiser <ekiser9@uncc.edu>
2019-11-08 16:06:02 +0000

    Created Generator controller and model, set up associations with activity

--- app/controllers/date_night_controller.rb
+++ app/controllers/date_night_controller.rb
@@ -1,11 +1,17 @@
 class DateNightController < ApplicationController
   
   def show
-    @date_night = DateNight.all
+    @date_night = Datenight.find(params[:id])
   end
 
-  def view
+  def new
+    @date_night = Datenight.new
+  end
+  
+  def create
+    @date_night = Datenight.new(datenight_params)
     
+    redirect_to @date_night
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get 'date_night/index'
   
+  resources :generator do
+    resources :activity
+  end
   
   resources :activity
   resources :usersubmission
