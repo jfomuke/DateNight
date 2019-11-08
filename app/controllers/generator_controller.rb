1f3d2e1550ee4b4da8a8c7a3d9be91f5f4657838
Elizabeth Kiser <ekiser9@uncc.edu>
2019-11-08 17:20:57 +0000

    Began updating views for Generator; Added notes to Home page

--- app/controllers/generator_controller.rb
+++ app/controllers/generator_controller.rb
@@ -8,9 +8,15 @@ class GeneratorController < ApplicationController
     end
     
     def create
-        @generator = Generator.new()
-        
-        redirect_to @generator
+        @generator = Generator.new(generator_params)
+        if @generator.save
+            redirect_to @generator
+        end
     end
     # has_and_belongs_to_many :activity - for use later in model
 end
+
+private
+    def generator_params
+        params.require(:generator).permit(:activityID, :foodID)
+    end
