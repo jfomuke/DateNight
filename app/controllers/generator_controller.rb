dbe2f322b182bc01a2cae98410910ae8c3156ee1
Elizabeth Kiser <ekiser9@uncc.edu>
2019-11-14 18:58:05 +0000

    Updated Generator Create parameters

--- app/controllers/generator_controller.rb
+++ app/controllers/generator_controller.rb
@@ -9,13 +9,23 @@ class GeneratorController < ApplicationController
     
     def create
         @generator = Generator.new(generator_params)
+        
         if @generator.save
             redirect_to @generator
         end
+        # return activity array
+        # choose random number to return one
+        
+        # return food array
+        # choose random number to return one
     end
 end
 
 private
     def generator_params
+        activityArray = Activity.where(category: 'activity').ids
+        :activityID = activityArray[rand(activityArray.length)]
+        foodArray = Activity.where(category: 'food').ids
+        :foodID = foodArray[rand(foodArray.length)]
         params.require(:generator).permit(:activityID, :foodID)
     end
