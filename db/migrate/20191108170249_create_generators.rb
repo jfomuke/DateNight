d020f4fbb1d983e49671ee88dd8c00b5579e31dc
Jason Fomuke <jfomuke@uncc.edu>
2019-11-21 18:43:47 +0000

    View All Button in nav bar now goes to Activity table not the UserSubmission table/page

--- db/migrate/20191108170249_create_generators.rb
+++ db/migrate/20191108170249_create_generators.rb
@@ -1,7 +1,7 @@
 class CreateGenerators < ActiveRecord::Migration[5.2]
   def change
     create_table :generators do |t|
-      t.integer :acitivityID
+      t.integer :activityID
       t.integer :foodID
 
       t.timestamps
