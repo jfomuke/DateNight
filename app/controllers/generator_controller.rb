e05f31a3582c80911df6c4c6c965281515c1e136
Elizabeth Kiser <ekiser9@uncc.edu>
2019-12-01 14:57:16 +0000

    Preliminary update to Generator controller to fix association issue

--- app/controllers/generator_controller.rb
+++ app/controllers/generator_controller.rb
@@ -9,11 +9,17 @@ class GeneratorController < ApplicationController
     end
     
     def create
-        activityArray = Activity.where(category: 'activity').ids
-        aID = activityArray[rand(activityArray.length)]
-        foodArray = Activity.where(category: 'food').ids
-        fID = foodArray[rand(foodArray.length)]
+        activityArray = Activity.where(category: 'activity')
+        randActivity = activityArray[rand(activityArray.length)]
+        randActivityParams = [:name => randActivity.name, :category => randActivity.category, :description => randActivity.description, :link => randActivity.link, :location => randActivity.location]
+        aID = randActivity.id
+        foodArray = Activity.where(category: 'food')
+        randFood = foodArray[rand(foodArray.length)]
+        fID = randFood.id
         @generator = Generator.new(acitivityID: aID, foodID: fID)
+        @generator.save
+        @activity = @generator.activity.create(randActivityParams)
+        @activity = @generator.activity.create(randFood.name, randFood.category, randFood.description, randFood.link, randFood.location)
         
         if @generator.save
             redirect_to @generator
