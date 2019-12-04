7d558b79b65938fa6ca9615ca317f8e20c66a9d7
Elizabeth Kiser <ekiser9@uncc.edu>
2019-12-04 16:18:29 +0000

    Added admin login for User Submission index

--- app/controllers/user_submission_controller.rb
+++ app/controllers/user_submission_controller.rb
@@ -1,4 +1,7 @@
 class UserSubmissionController < ApplicationController
+    
+    http_basic_authenticate_with :name => "admin", :password => "password", only: :index
+    
     def index
         @usersubmission = UserSubmission.all
     end
